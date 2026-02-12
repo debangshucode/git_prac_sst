d40a07b764167c5278c216236b35ff1e0c5fc853
Debangshu Dey <deydebangshu51@gmail.com>
2026-02-12 09:55:25 +0530

    Js arithmetic expressions

---
+++ script.js
@@ -0,0 +1,24 @@
+let a =10 
+let b =20
+console.log("Addition of a+b = " + (a+b))
+console.log("multiplication of a*b = " + (a*b))
+console.log("division of a/b = " + (a/b))
+console.log("subtraction of a-b = " + (a-b))
+if(a>b){
+    console.log("a is greater than b")
+}else{
+    console.log("a is not greater than b")
+}
+if(a<b){
+    console.log("a is less than b")
+}else{
+    console.log("a is not less than b")
+}
+if(a==b){
+    console.log("a is equal to b")
+}else{    console.log("a is not equal to b")
+}
+console.log("remainder of a%b = " + (a%b))
+console.log("power of a**b = " + (a**b))
+console.log("increment of a = " + (++a))
+console.log("decrement of a = " + (--a))

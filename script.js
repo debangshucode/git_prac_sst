0c32517275ae173ca18e62cccfff65570eb4437e
Debangshu Dey <deydebangshu51@gmail.com>
2026-02-12 09:59:37 +0530

    js operators

--- script.js
+++ script.js
@@ -18,6 +18,14 @@ if(a==b){
     console.log("a is equal to b")
 }else{    console.log("a is not equal to b")
 }
+if(a!=b){
+    console.log("a is not equal to b")
+}
+if(a===b){
+    console.log("a is strictly equal to b")
+}else{
+    console.log("a is not strictly equal to b")
+}
 console.log("remainder of a%b = " + (a%b))
 console.log("power of a**b = " + (a**b))
 console.log("increment of a = " + (++a))

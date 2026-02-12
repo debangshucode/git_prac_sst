30a7c1682ebffd243811b89a9d506d1df8314119
Debangshu Dey <deydebangshu51@gmail.com>
2026-02-12 10:46:08 +0530

    for loop

--- script.js
+++ script.js
@@ -67,3 +67,11 @@ console.log(flag);
 
 (day === "sunday" && age >=18) ? console.log("you can go out") : console.log("you cannot go out");
 (day === "sunday" || age >=18) ? console.log("you can go out") : console.log("you cannot go out");
+
+let car = new Array(10);
+console.log(car.length);
+
+for (let i=0;i<car.length;i++){
+    car[i]=i;
+    console.log(car[i]);
+}

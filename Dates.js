1909d2d8dd6aec0b8faa9b1350cf93966073fb2d
Debangshu Dey <deydebangshu51@gmail.com>
2026-02-13 10:57:36 +0530

    TemporalDates

--- Dates.js
+++ Dates.js
@@ -4,3 +4,23 @@ console.log(  new Date().toUTCString() );
 console.log( new Date().toISOString() );
 console.log(new Date().getDay());
 
+console.log(Temporal.Now.zonedDateTimeISO().second);
+const myDate = Temporal.PlainDate.from('2024-06-01');
+
+const newDate = myDate.subtract({ days: 10 });
+
+console.log(newDate.toString()); // Output: 2024-05-22
+
+
+const wedding = Temporal.PlainDate.from('2026-05-01');
+const today = Temporal.Now.plainDateISO();
+
+const duration = today.until(wedding);
+console.log(duration.toString()); 
+
+// Create a Temporal object
+const date = Temporal.PlainDate.from("2026-05-01");
+
+// Replace month and day
+const customDate = date.with({ month:12, day:25 });
+console.log(customDate.toString()); 

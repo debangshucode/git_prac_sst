45cb4e029be1885e4e5769593c10e2efc843d9fb
Debangshu Dey <deydebangshu51@gmail.com>
2026-02-13 10:22:25 +0530

    js Dates

---
+++ Dates.js
@@ -0,0 +1,6 @@
+console.log( Date.parse( new Date())  );
+console.log(  new Date().toDateString() );
+console.log(  new Date().toUTCString() );
+console.log( new Date().toISOString() );
+console.log(new Date().getDay());
+

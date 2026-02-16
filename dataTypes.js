431e1adab57f0886e3d80fac880e478345c0e214
Debangshu Dey <deydebangshu51@gmail.com>
2026-02-16 11:04:00 +0530

    dataTypes

---
+++ dataTypes.js
@@ -0,0 +1,24 @@
+// Built-In Object Types
+// A JavaScript object can represent a JavScript object or a User defined object.
+
+// Built-in JavavaScript object types can be:
+
+// Object	Description
+// Array	Array of values accessed by a numerical index
+// Map	Array of key-value pairs where the keys can be of any data type
+// Set	Array of values where each value can only appear once
+// WeakMap	A type of Map with weak references to the stored objects.
+// WeakSet	A type of Set with weak references to the stored objects.
+// Math	An object that provides math constants and functions like PI and random()
+// Date	Object for working with dates and times
+// RegExp	Object for working with regular expressions
+// Error	Object represents error conditions during program execution
+// JSON	Object with methods for parsing values between JSON and objects
+// Promise	Object representing the completion or failure of an asynchronous operation
+// Int8Array	Array for storing fixed-size 8-bits integer values
+// Int16Array	Array for storing fixed-size 16-bits integer values
+// Int32Array	Array for storing fixed-size 32-bits integer values
+// Float16Array	Array for storing fixed-size 16-bits floating-point values
+// Float32Array	Array for storing fixed-size 32-bits floating-point values
+// Float64Array	Array for storing fixed-size 64-bits floating-point values
+// BigInt64Array	Array for storing fixed-size 64-bits big integer values

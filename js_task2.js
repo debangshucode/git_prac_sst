2cc608bae78ad7d6e63b9b19c819e2d8d2ed64d1
Debangshu Dey <deydebangshu51@gmail.com>
2026-02-17 11:25:08 +0530

    task 2 question 1

---
+++ js_task2.js
@@ -0,0 +1,29 @@
+//Q1 You are given an array of numbers. Filter out all numbers less than or equal to 10, and then double each of the remaining numbers. Return the final array.
+let numbers = [5, 12, 3, 20, 8, 15];
+let result = numbers.filter(num => num > 10).map(num => num * 2);
+console.log(`Q1:  ${result}`);
+//Q2 Write a function that takes a string and returns it reversed.
+
+//Q3 You have an array of objects where each object contains a  name and a score. Sort the array in descending order of score.
+
+//Q4 Given a sentence, count how many times each word appears and return the result as an object where keys are words and values are their counts.
+
+//Q5 Write a function that checks if a given string is a palindrome. The check should be case-insensitive and should ignore spaces.
+
+//Q6 You have an array containing duplicate values. Remove all duplicates without using set and return the cleaned array.
+
+//Q7 Write a function that splits an array into chunks of a given size and returns them as an array of arrays.
+
+//Q8 Given a sentence, return a new sentence where the first letter of every word is capitalized.
+
+//Q9 Convert an object into an array of key, value pairs.
+
+//Q10 Given a string, count how many times each character appears (ignoring spaces) and return the result as an object.
+
+//Q11 Flatten a deeply nested array into a single-level array.
+
+//Q12 Write a function that finds the largest and smallest number in an array.
+
+//Q13 Given two arrays, return a new array containing only the elements that are present in both.
+
+ 
